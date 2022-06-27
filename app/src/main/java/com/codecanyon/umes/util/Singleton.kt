c3ddcfa2dbd7b5490e8aada1a3470f5b83f2b5fa
MustafaSalihEybek <53754841+MustafaSalihEybek@users.noreply.github.com>
2022-06-27 10:18:32 +0300

    Bu sınıfta yeni yazılan repository sınıfları tanımlandı

--- app/src/main/java/com/codecanyon/umes/util/Singleton.kt
+++ app/src/main/java/com/codecanyon/umes/util/Singleton.kt
@@ -5,15 +5,17 @@ import android.content.Context
 import android.view.View
 import android.widget.RelativeLayout
 import com.codecanyon.umes.model.Coordinator
+import com.codecanyon.umes.model.InsDirector
 import com.codecanyon.umes.model.coordinator.StudentCoordinator
 import com.codecanyon.umes.view.dialog.CoordinatorStudentDetailsDialog
 import com.codecanyon.umes.view.dialog.ExitAppDialog
 import com.codecanyon.umes.view.dialog.UpdateCoordinatorAndDepDirectorProfileDialog
+import com.codecanyon.umes.view.dialog.UpdateInstitutionDirectorProfileDialog
 import com.codecanyon.umes.viewmodel.MainViewModel
 
 class Singleton {
     companion object{
-        val SITE_URL: String = "192.168.1.21"
+        val SITE_URL: String = "192.168.27.3"
         val BASE_URL: String = "http://${SITE_URL}/UMES/api/"
 
         val VERTICAL_ITEM_SIZE: Int = 15
@@ -21,6 +23,7 @@ class Singleton {
         lateinit var coordinatorStudentDetailsDialog: CoordinatorStudentDetailsDialog
         lateinit var exitAppDialog: ExitAppDialog
         var updateCoordinatorAndDepDirectorProfileDialog: UpdateCoordinatorAndDepDirectorProfileDialog? = null
+        var updateInstitutionDirectorProfileDialog: UpdateInstitutionDirectorProfileDialog? = null
 
         @SuppressLint("StaticFieldLeak")
         var mNavView: RelativeLayout? = null
@@ -59,6 +62,19 @@ class Singleton {
 
         fun closeUpdateCoordinatorAndDepDirectorProfileDialog(){
             updateCoordinatorAndDepDirectorProfileDialog?.let {
+                if (it.isShowing)
+                    it.dismiss()
+            }
+        }
+
+        fun showUpdateInstitutionDirectorProfileDialog(mContext: Context, insDirector: InsDirector){
+            updateInstitutionDirectorProfileDialog = UpdateInstitutionDirectorProfileDialog(mContext, insDirector)
+            //updateInstitutionDirectorProfileDialog!!.setCancelable(false)
+            updateInstitutionDirectorProfileDialog!!.show()
+        }
+
+        fun closeUpdateInstitutionDirectorProfileDialog(){
+            updateInstitutionDirectorProfileDialog?.let {
                 if (it.isShowing)
                     it.dismiss()
             }
