d4569cc0d7f7803b24ccfd7a8fe592cee3d88dbb
MustafaSalihEybek <53754841+MustafaSalihEybek@users.noreply.github.com>
2022-06-27 10:19:06 +0300

    Kurum yetkilisine görelik öğrenciler verisi bu sınıfta saklandı

--- app/src/main/java/com/codecanyon/umes/model/institutiondirector/StudentsInstitutionDirector.kt
+++ app/src/main/java/com/codecanyon/umes/model/institutiondirector/StudentsInstitutionDirector.kt
@@ -2,4 +2,10 @@ package com.codecanyon.umes.model.institutiondirector
 
 data class StudentsInstitutionDirector (
     val studentId: String = "",
+    val studentName: String = "",
+    val studentSurname: String = "",
+    val studentEmail: String = "",
+    val studentPhone: String = "",
+    val studentCoordinatorId: Int = 1,
+    val studentCoordinatorName: String = ""
 )
