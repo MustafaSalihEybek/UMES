f4ee46d7f9a9aed0a2a5d6158a25170499d41a41
MustafaSalihEybek <53754841+MustafaSalihEybek@users.noreply.github.com>
2022-06-27 10:14:52 +0300

    Kurum yetkilisinin öğrencileri görmesi için görünüm ile veriler arasındaki bağlantı için bu class oluşturuldu

--- app/src/main/java/com/codecanyon/umes/view/institutiondirector/InstitutionDirectorStudentsFragment.kt
+++ app/src/main/java/com/codecanyon/umes/view/institutiondirector/InstitutionDirectorStudentsFragment.kt
@@ -5,13 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.codecanyon.umes.R
+import com.codecanyon.umes.adapter.decoration.LinearManagerDecoration
+import com.codecanyon.umes.adapter.institutiondirector.InstitutionDirectorStudentsAdapter
+import com.codecanyon.umes.model.institutiondirector.StudentsInstitutionDirector
+import com.codecanyon.umes.util.Singleton
+import com.codecanyon.umes.util.show
+import com.codecanyon.umes.viewmodel.institutiondirector.InstitutionDirectorStudentsViewModel
+import kotlinx.android.synthetic.main.fragment_institution_director_students.*
 
-class InstitutionDirectorStudentsFragment : Fragment() {
+class InstitutionDirectorStudentsFragment(val institutionDirectorId: Int) : Fragment() {
     private lateinit var v: View
+    private lateinit var institutionDirectorStudentsViewModel: InstitutionDirectorStudentsViewModel
+
+    private lateinit var studentsAdapter: InstitutionDirectorStudentsAdapter
+    private lateinit var studentList: List<StudentsInstitutionDirector>
 
     private fun init(){
+        institution_director_students_fragment_recyclerView.setHasFixedSize(true)
+        institution_director_students_fragment_recyclerView.layoutManager = LinearLayoutManager(v.context, LinearLayoutManager.VERTICAL, false)
+        studentsAdapter = InstitutionDirectorStudentsAdapter(arrayListOf())
+        institution_director_students_fragment_recyclerView.adapter = studentsAdapter
 
+        institutionDirectorStudentsViewModel = ViewModelProvider(this).get(InstitutionDirectorStudentsViewModel::class.java)
+        observeLiveData()
+        institutionDirectorStudentsViewModel.getStudents(institutionDirectorId)
     }
 
     override fun onCreateView(
@@ -26,5 +47,25 @@ class InstitutionDirectorStudentsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         v = view
         init()
+    }
+
+    private fun observeLiveData(){
+        institutionDirectorStudentsViewModel.errorMessage.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                it.show(v, it)
+            }
+        })
+
+        institutionDirectorStudentsViewModel.studentList.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                studentList = it
+
+                if (institution_director_students_fragment_recyclerView.itemDecorationCount > 0)
+                    institution_director_students_fragment_recyclerView.removeItemDecorationAt(0)
+
+                institution_director_students_fragment_recyclerView.addItemDecoration(LinearManagerDecoration(Singleton.VERTICAL_ITEM_SIZE, it.size))
+                studentsAdapter.loadData(it)
+            }
+        })
     }
 }
